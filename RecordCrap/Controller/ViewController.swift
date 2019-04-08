3fd98fe550c88a5ddcfb3e7c31bb7cd2bd2330b8
Skullbonk <reagan.fairbanks@gmail.com>
2019-04-08 13:55:44 -0600

    Initial commit

---
+++ RecordCrap/Controller/ViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ViewController.swift
+//  RecordCrap
+//
+//  Created by Fairbanks, Reagan on 4/8/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController
+{
+	var audioRecorder:AVAudioRecorder!
+	
+	func record()
+	{
+		
+	}
+	
+	@IBAction func recordButtonTap(_ sender: UIButton)
+	{
+		
+	}
+	
+	
+	@IBAction func sendButtonTap(_ sender: UIButton)
+	{
+		
+	}
+	
+
+}
+
