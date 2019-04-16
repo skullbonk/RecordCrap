b235531a9fd5d71d4b6d2badf6460944217ce303
Skullbonk <reagan.fairbanks@gmail.com>
2019-04-16 13:56:09 -0600

    confuse

--- RecordCrap/Controller/ViewController.swift
+++ RecordCrap/Controller/ViewController.swift
@@ -11,20 +11,82 @@ import AVFoundation
 
 class ViewController: UIViewController
 {
-	var audioRecorder:AVAudioRecorder?
-	var audioPlayer:AVAudioPlayer?
-	var alertController:UIAlertController?
-	
-	@IBOutlet weak var sendButtonOut: UIButton!
-	
-	@IBOutlet weak var playButtonOut: UIButton!
+	var audioRecorder: AVAudioRecorder!
+	var audioSession: AVAudioSession!
+	var audioPlayer: AVAudioPlayer!
+	var alertController: UIAlertController!
 	
 	@IBOutlet weak var recordButtonOut: UIButton!
 	
+	@IBOutlet weak var playButtonOut: UIButton!
+	
+	@IBOutlet weak var sendButtonOut: UIButton!
+	
+	var recordButton: UIButton!
+	var playButton: UIButton!
+	var sendButton: UIButton!
+	
+	override func viewDidLoad()
+	{
+		super.viewDidLoad()
+		
+
+		do
+		{
+			try audioSession.setCategory(.playAndRecord, mode: .default)
+			try audioSession.setActive(true)
+			audioSession.requestRecordPermission()
+				{ [unowned self] allowed in DispatchQueue.main.async
+					{
+						if allowed
+						{
+							self.loadRecordingUI()
+						}
+						else
+						{
+							print("frig it brokje")
+						}
+					}
+			}
+		}
+		catch
+		{
+			print("ah rats is busted")
+		}
+	}
+	
+	func loadRecordingUI()
+	{
+		recordButton = recordButtonOut
+		playButton = playButtonOut
+		sendButton = sendButtonOut
+	}
+	
+	func getDocumentsDirectory() -> URL
+	{
+		let paths = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+		return paths[0]
+	}
+	
 	func record()
 	{
-		audioRecorder.record()
+		let audioFilename = getDocumentsDirectory().appendingPathComponent("recording.wav")
 		
+		let settings = [
+			AVFormatIDKey:
+		]
+		
+		do
+		{
+			audioRecorder = try AVAudioRecorder(url: audioFilename, settings: settings)
+			audioRecorder.record()
+			
+			recordButton.setTitle("Stop", forL .normal)
+		}
+		catch
+		{
+			finishRecording(success: false)
+		}
 	}
 	
 	func play()
@@ -45,18 +107,25 @@ class ViewController: UIViewController
 	
 	@IBAction func sendButtonTouch(_ sender: UIButton)
 	{
-		let activityController = UIActivityViewController(activityItems: [recordButtonOut.], applicationActivities: nil)
-		present(activityController, animated: true, completion: nil)
+		
 	}
 	
 	@IBAction func playButtonTouch(_ sender: UIButton)
 	{
-		
+	
 	}
 	
 	@IBAction func recordButtonTouch(_ sender: UIButton)
 	{
-		audioRecorder.record()
+		
+		if(!audioRecorder.isRecording)
+		{
+			audioRecorder.record()
+		}
+		else
+		{
+			audioRecorder.pause()
+		}
 		
 	}
 	
